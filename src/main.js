66750211a947d83def30d9f79c5d69ade3cf1585
cm <connormccarthy8@gmail.com>
2018-10-19 09:42:55 -0700

    successfull call api and display the doctors names

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import { Search } from "./search.js";
 
 $(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    $("#results").empty();
 
+    // let location = $("#location").val();
+
+    let newSearch = new Search();
+    let promise = newSearch.getDoctors();
+
+    promise.then(
+      function(response) {
+        let body = JSON.parse(response);
+        for (let i = 0; i < body.data.length; i++) {
+          $("#results").append(
+            `<h3>${body.data[i].profile.first_name} ${
+              body.data[i].profile.last_name
+            }</h3>`
+          );
+        }
+      },
+      function(error) {
+        $(".showErrors").text(
+          `There was an error processing your request: ${error.message}`
+        );
+      }
+    );
+  });
 });
