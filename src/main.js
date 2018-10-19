3c6421a1c67aa6530c1afcccffa17dec9458ceb3
cm <connormccarthy8@gmail.com>
2018-10-19 10:18:49 -0700

    program return the doctors name, address and if they are accepting new patients

--- src/main.js
+++ src/main.js
@@ -21,8 +21,25 @@ $(document).ready(function() {
           $("#results").append(
             `<h3>${body.data[i].profile.first_name} ${
               body.data[i].profile.last_name
-            }</h3>`
+            }</h3> <h4>${body.data[i].practices[0].visit_address.street} ${
+              body.data[i].practices[0].visit_address.city
+            } ${body.data[i].practices[0].visit_address.state} ${
+              body.data[i].practices[0].visit_address.zip
+            }</h4> <h5>${body.data[i].practices[0].phones[0].number}</h5> <h6>${
+              body.data[i].practices[0].accepts_new_patients
+            }</h6>`
           );
+          //   $("#results-table").append(
+          //     `<tr>
+          //     <td>${body.data[i].profile.first_name} ${
+          //       body.data[i].profile.last_name
+          //     }</td>
+          //     <td>${body.data[i].visit_address}</td>
+          //     // <td>${body.bikes[i].frame_colors[0]}</td>
+          //     // <td>${body.bikes[i].year}</td>
+          //     // <td>${body.bikes[i].manufacturer_name}</td>
+          //   </tr>`
+          //   );
         }
       },
       function(error) {
